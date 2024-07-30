8588f9a6ba9b3a69315363a5ea793291183c6bda
HuyDang92 <huy922003@gmail.com>
2024-07-31 00:03:39 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,125 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import Header from "./components/Header";
+import Skills from "./components/Skills";
+import gsap from "gsap-trial";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
+import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import Earth from "./components/Earth";
 
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
+const moveTo = (id: number) => {
+   switch (id) {
+      case 1:
+         return ".aboutMe";
+      case 2:
+         return ".story";
+      case 3:
+         return ".skills";
+      case 4:
+         return ".experience";
+      case 5:
+         return ".products";
+      case 6:
+         return ".interests";
+      case 7:
+         return ".contact";
+   }
+};
 function App() {
+   const main = useRef<any>();
+   const smoother = useRef<any>();
+   const scrollTo = (id: number) => {
+      smoother.current.scrollTo(moveTo(id), true, "center center");
+   };
+
+   useGSAP(
+      () => {
+         smoother.current = ScrollSmoother.create({
+            smooth: 1.5,
+            effects: true,
+         });
+      },
+      { scope: main }
+   );
+
    return (
-      <div className="">
-         <Header />
-         <div className="w-[1400px] relative mx-auto h-[600px] mb-20">
-            <img src="/banner.webp" alt="" className="rounded-3xl h-full w-full object-cover" />
-            <div className="absolute top-10 w-full z-0 text-center left-1/2 -translate-x-1/2 text-white">
-               <h1 className="text-7xl ">Hi, I'm Dang huynh huy</h1>
-               <p className="text-xl mt-2">I'm a front-end developer</p>
-            </div>
-         </div>
-         <div className="aboutMe">
-            <div className="absolute h-[470px] overflow-hidden flex w-full z-0 text-white">
-               <div className="w-[60%] p-16">
-                  <h1 className="text-6xl mb-5">About me</h1>
-                  <p className="h-[270px] overflow-y-auto font-sans font-semibold text-lg">
-                     I enjoy innovating more than anything, and I learn through the challenges of creating. I graduated with a BS in Computer Science
-                     in 2016 and soon after became a full-stack engineer, which opened the door to pursuing my passions and interests. I love
-                     expanding my knowledge of growing technologies - whether they lie in software, mechanics, or electronics - and applying it to
-                     building existing and innovative solutions. Through doing what I love I've become great at what I do. Co-founding two startups
-                     has given me more experience across all facets of business and product development than I could've ever hoped for. Not only have
-                     I learned from my individual mistakes and accomplishments, but the guidance from many differing perspectives has been extremely
-                     valuable to my growth. Throughout my professional career I've been heavily involved in planning and executing core business
-                     strategies / marketing efforts as well as facilitating the growth of invaluable partnerships and networks. But more importantly,
-                     I've excelled at architecting full-stack (MERN) applications, from conceptualization to users and revenue, always with
-                     scalability and user experience in mind. I love connecting with great people, hearing new perspectives, and exploring creative
-                     ideas, so please reach out to me (me@marcoprouve.com) if you want to talk about upcoming opportunities, growing technologies, or
-                     even just want to chat.
-                  </p>
-                  <div className="flex items-center mt-4 gap-3">
-                     <Icon icon="ph:mouse-simple-bold" className="text-2xl" />
-                     <span className="">SCROLL</span>
+      <>
+         <Header callBack={scrollTo} />
+         <div id="smooth-wrapper" ref={main}>
+            <div className="mt-[90px]" id="smooth-content">
+               <div className="w-[1400px] relative z-0 mx-auto h-[600px] mb-20">
+                  <img src="/banner.webp" alt="" className="rounded-3xl h-full w-full object-cover" />
+                  <div className="absolute top-10 w-full z-0 text-center left-1/2 -translate-x-1/2 text-white">
+                     <h1 className="text-7xl ">Hi, I'm Dang huynh huy</h1>
+                     <p className="text-xl mt-2">I'm a front-end developer</p>
                   </div>
                </div>
-               <img src="/avt.png" className="" alt="" />
-            </div>
-         </div>
-         <div className="my-20 w-[75rem] relative mx-auto">
-            <h1 className="text-6xl text-center">Story</h1>
-            <p className="text-center">Interact with the map below to uncover my story</p>
-            <div className="w-full flex mt-14">
-               <div className="w-1/2"></div>
-               <div className="w-1/2 text-[#1D232A] space-y-2 rounded-3xl bg-gradient-to-b from-[#E6F9FF] to-white p-10">
-                  <p>
-                     <h3 className="text-3xl">Location</h3>
-                     <p className="font-sans font-semibold">Go Vap, Ho Chi Minh</p>
-                  </p>
-                  <p>
-                     <h3 className="text-3xl">When</h3>
-                     <p className="font-sans font-semibold">2021 - Present</p>
-                  </p>
-                  <p>
-                     <h3 className="text-3xl">Description</h3>
-                     <p className="font-sans font-semibold">
-                        I'm currently living in Marina Del Rey, CA, having moved here in 2020 during the pandemic. With the beach being so close,
-                        adventures at every corner, and sunshine year-round, I couldn't be happier being where I am.
-                     </p>
-                  </p>
+               <div className="aboutMe">
+                  <div className="absolute h-[470px] overflow-hidden flex w-full z-0 text-white">
+                     <div className="w-[60%] p-16">
+                        <h1 className="text-6xl mb-5">About me</h1>
+                        <p className="h-[270px] overflow-y-auto font-sans font-semibold text-lg">
+                           I enjoy innovating more than anything, and I learn through the challenges of creating. I graduated with a Software Engineer
+                           and soon after became a Front-end engineer, which opened the door to pursuing my passions and interests. I love expanding
+                           my knowledge of growing technologies. Through doing what I love I've become great at what I do. I've excelled at
+                           architecting Web applications, always with scalability and user experience in mind. I love connecting with great people,
+                           hearing new perspectives, and exploring creative ideas, so please reach out to me (danghuynhhuy@gmail.com) if you want to
+                           talk about upcoming opportunities, growing technologies, or even just want to chat.
+                        </p>
+                        <div className="flex items-center mt-4 gap-3">
+                           <Icon icon="ph:mouse-simple-bold" className="text-2xl" />
+                           <span className="">SCROLL</span>
+                        </div>
+                     </div>
+                     <img src="/me.png" className="" alt="" />
+                  </div>
                </div>
+               <div className="story my-20 w-[75rem] relative mx-auto">
+                  <h1 className="text-6xl text-center">Story</h1>
+                  <p className="text-center">Interact with the map below to uncover my story</p>
+                  <div className="w-full flex mt-14">
+                     <div className="w-1/2">
+                        <Earth />
+                     </div>
+                     <div className="w-1/2 text-[#1D232A] space-y-2 rounded-3xl bg-gradient-to-b from-[#E6F9FF] to-white p-10">
+                        <p>
+                           <h3 className="text-3xl">Location</h3>
+                           <p className="font-sans font-semibold">Go Vap, Ho Chi Minh</p>
+                        </p>
+                        <p>
+                           <h3 className="text-3xl">When</h3>
+                           <p className="font-sans font-semibold">2021 - Present</p>
+                        </p>
+                        <p>
+                           <h3 className="text-3xl">Description</h3>
+                           <p className="font-sans font-semibold">
+                              I'm currently living in Marina Del Rey, CA, having moved here in 2020 during the pandemic. With the beach being so
+                              close, adventures at every corner, and sunshine year-round, I couldn't be happier being where I am.
+                           </p>
+                        </p>
+                     </div>
+                  </div>
+               </div>
+               <Skills />
+               <div className="pt-40">
+                  <div className="contact rounded-3xl text-white text-center p-20 flex flex-col justify-center">
+                     <h1 className="text-6xl ">Contact Me</h1>
+                     <p className="font-sans w-[80%] mx-auto my-5">
+                        If you are looking for someone to fill an open full-stack position or would simply like to consult me on full-stack
+                        engineering decisions, please do not hesitate to contact me.
+                     </p>
+                     <div className="flex gap-4 items-center justify-center">
+                        <button className="p-3 w-[20rem] border-4 rounded-md bg-transparent focus:outline-none">danghuynhhuy776@gmail.com</button>
+                        <button className="p-3 w-[20rem] border-4 rounded-md bg-white text-[#FF5858] focus:outline-none">RESUME</button>
+                     </div>
+                  </div>
+               </div>
+               <div className="pt-40"></div>
             </div>
          </div>
-      </div>
+      </>
    );
 }
 
---
+++ src/components/Skills.tsx
@@ -0,0 +1,115 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const titles = [
+   {
+      id: 1,
+      name: "Technical",
+      icon: "mingcute:code-fill",
+   },
+   {
+      id: 2,
+      name: "Frameworks",
+      icon: "iconoir:database-solid",
+   },
+   {
+      id: 3,
+      name: "Libraries",
+      icon: "fluent:library-16-filled",
+   },
+   {
+      id: 4,
+      name: "Tools",
+      icon: "carbon:branch",
+   },
+   {
+      id: 5,
+      name: "Project Management",
+      icon: "heroicons:cpu-chip-20-solid",
+   },
+   {
+      id: 6,
+      name: "Others",
+      icon: "mdi:tag-plus",
+   },
+];
+const technical = ["HTML5", "CSS3", "SCSS", "JavaScript", "TypeScript", "PHP", "NoSQL"];
+const frameworks = ["ReactJS", "NextJS", "Angular"];
+const libraries = [
+   "Redux",
+   "Redux Toolkit",
+   "Bootstrap",
+   "TailwindCSS",
+   "Material-UI",
+   "Ant Design",
+   "FireBase",
+   "Animation GSAP",
+   "WebRTC",
+   "SocketIO",
+];
+const projectManagement = ["GitHub", "Trello", "Google Drive"];
+const tools = ["Git", "VS Code", "Figma", "Postman"];
+const others = ["English (Toeic 650)", "Team work ", "Responsive Design", "Restful API"];
+
+const switchSkills = (id: number) => {
+   switch (id) {
+      case 1:
+         return technical;
+      case 2:
+         return frameworks;
+      case 3:
+         return libraries;
+      case 4:
+         return tools;
+      case 5:
+         return projectManagement;
+      case 6:
+         return others;
+   }
+};
+function Skills() {
+   const [active, setActive] = useState<number>(1);
+   return (
+      <div className="skills mt-20 w-[75rem] relative mx-auto p-8 py-12 flex gap-10 items-start rounded-3xl bg-[#25262A]">
+         <div className="w-1/2 flex flex-col gap-1.5">
+            {titles.map((title) => (
+               <div
+                  key={title?.id}
+                  onClick={() => setActive(title?.id)}
+                  className={`${
+                     active === title?.id ? "py-7 text-[#FF7777]" : "text-white"
+                  } duration-300 px-6 py-4 text-xl  rounded-xl bg-[#2D2E33] flex justify-between items-center`}
+               >
+                  <span>{title?.name}</span>
+                  <Icon icon={title?.icon} />
+               </div>
+            ))}
+         </div>
+         <div className="w-1/2 text-white">
+            <h1 className="text-6xl">Skills</h1>
+            <div className="flex gap-3 flex-wrap mt-5">
+               {switchSkills(active)?.map((skill, index) => (
+                  <motion.div
+                     initial={{ scale: 0.5 }}
+                     animate={{ scale: 1 }}
+                     transition={{
+                        delay: 0,
+                        duration: 0.3,
+                        type: "spring",
+                        stiffness: 250,
+                        damping: 20,
+                     }}
+                     key={index}
+                     className="rounded-lg p-2 py-1 border-2 border-white"
+                  >
+                     {skill}
+                  </motion.div>
+               ))}
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Skills;
